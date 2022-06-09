2840cd73ab34a162feba5eec929d8cd4c78f0fcd
Rukanda Faridsi <rukanda.faridsi@bukalapak.com>
2022-06-09 11:51:45 +0700

    Fix infinite scroll component

--- src/components/InfiniteScroll/index.js
+++ src/components/InfiniteScroll/index.js
@@ -1,18 +1,25 @@
 export default function InfiniteScroll(props) {
   window.onscroll = () => {
-    if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-      if(props.nextListUrl) {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      if (props.nextListUrl) {
         props.fetchNextPage();
       }
     }
-  }
+  };
 
   return (
     <>
       <hr />
-      <div className="text-center p-3 bg-[#333] rounded text-white mt-5">
-        {props.isLoading ? "LOADING DATA..." : "CLICK TO LOAD MORE"}
-      </div>
+      {props.nextListUrl && (
+        <div className="text-center p-3 bg-[#333] rounded text-white mt-5">
+          {props.isLoading
+            ? "LOADING DATA..."
+            : "IF YOU CAN READ THIS, THERE IS SOMETHING WRONG WITH OUR INFINITE SCROLL"}
+        </div>
+      )}
     </>
   );
 }
